615c5bb7986b982ea3acc109005f1797a70b1c4b
Aleksandr Kucherenko <49811827+G28XYZ@users.noreply.github.com>
2022-02-14 12:26:26 +0700

    Update clock.js

--- clock.js
+++ clock.js
@@ -3,14 +3,17 @@
     let sec = now.getSeconds();                 // Seconds
     let min = now.getMinutes() + sec/60;        // Fractional minutes
     let hour = (now.getHours() % 12) + min/60;  // Fractional hours
+    let secangle = sec * 6;                     // 6 degrees per minute
     let minangle = min * 6;                     // 6 degrees per minute
     let hourangle = hour * 30;                  // 30 degrees per hour
 
     // Get SVG elements for the hands of the clock
+    let sechand = document.querySelector("#clock .secondhand");
     let minhand = document.querySelector("#clock .minutehand");
     let hourhand = document.querySelector("#clock .hourhand");
 
     // Set an SVG attribute on them to move them around the clock face
+    sechand.setAttribute("transform", `rotate(${secangle},50,50)`);
     minhand.setAttribute("transform", `rotate(${minangle},50,50)`);
     hourhand.setAttribute("transform", `rotate(${hourangle},50,50)`);
 
